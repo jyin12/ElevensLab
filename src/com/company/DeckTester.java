285d28ac9d28fab588fede583ee9c61715709280
Jiayi  Yin <jy412@pvhs-lab112-d15.pvsd.org>
2017-03-16 10:04:47 -0400

    Initial commit

---
+++ src/com/company/DeckTester.java
@@ -0,0 +1,18 @@
+package com.company;
+
+/**
+ * Created by jy412 on 3/16/17.
+ */
+public class DeckTester {
+    /**
+     * This is a class that tests the Deck class.
+     */
+        /**
+         * The main method in this class checks the Deck operations for consistency.
+         *	@param args is not used.
+         */
+        public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+        }
+    }
+
